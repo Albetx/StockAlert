03ace4dd3f9d50e0f3cacde61b831764a2dbe112
Albetx <albetx@gmail.com>
2022-08-21 19:28:10 +0300

    Update News to use different sort type depending on the type of update

--- News.py
+++ News.py
@@ -2,6 +2,7 @@ import requests
 from datetime import date
 
 NEWS_API = "87d20ac742a04bbb9b8610f2e846c61c"
+DRAMATIC_CHANGE_CODE = 100
 
 
 class News:
@@ -9,13 +10,19 @@ class News:
     def __init__(self, ticker_symbol):
         self.ticker = ticker_symbol
 
-    def get_news(self, number_of_articles) -> [str]:
+
+    def get_news(self, number_of_articles, update_code: int) -> [str]:
+
+        if update_code == DRAMATIC_CHANGE_CODE:
+            sort_by = "popularity"
+        else:
+            sort_by = "relevancy"
 
         yesterday = date(date.today().year, date.today().month, date.today().day - 1)
         parameters = {
             "q": self.ticker,
             "from": yesterday,
-            "sortBy": "popularity",
+            "sortBy": sort_by,
             "apiKey": NEWS_API
         }
 
