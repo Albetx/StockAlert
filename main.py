2e2b35672cb75d8c6e0d1f332544b2d54b1de128
Albetx <albetx@gmail.com>
2022-08-23 14:25:34 +0300

    Add loop to track all the tickers from the file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Tracking import *
 from TrackingInvest import *
+from TrackingTrade import *
 import pandas
 from datetime import datetime
 
@@ -8,16 +9,31 @@ NO_SIG_CHANGE = -999
 
 
 try:
-    with open("tickers.txt") as tickers_list:
-        tickers = pandas.read_csv(tickers_list)
-        tickers_list = tickers[tickers["Group"] == "Invest"]["Ticker"].values
+    with open("tickers.txt") as tickers_list_Invest:
+        tickers_table = pandas.read_csv(tickers_list_Invest)
+        tickers_list_Invest = tickers_table[tickers_table["Group"] == "Invest"]["Ticker"].values
+        tickers_list_Trade = tickers_table[tickers_table["Group"] == "Trade"]["Ticker"].values
+        tickers_list_Track = tickers_table[tickers_table["Group"] == "Track"]["Ticker"].values
 
 except FileNotFoundError:
-    print ("No ticker file found..")
+    print("No ticker file found..")
 
 else:
-    t1 = TrackingInvest("TSLA")
-    t1.check_changes()
+    # Invest group
+    for ticker in tickers_list_Invest:
+        t1 = TrackingInvest(ticker)
+        t1.check_changes()
+        t1.periodic_update()
+    # Trade group
+    for ticker in tickers_list_Trade:
+        t1 = TrackingTrade(ticker)
+        t1.check_changes()
+        t1.periodic_update()
+    # Track group
+    for ticker in tickers_list_Track:
+        t1 = Tracking(ticker)
+        t1.check_changes()
+        t1.periodic_update()
 
-# TODO: Test every ticker from the file for updates
+
 # TODO: Add UI
