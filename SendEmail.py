aba5039ef198c1b4586ad84c4bbe77f165dfff5e
Albetx <albetx@gmail.com>
2022-08-23 13:01:08 +0300

    Fix some bugs with mail massegs

--- SendEmail.py
+++ SendEmail.py
@@ -15,21 +15,21 @@ class SendEmail:
         self.mail_addr = mail_addr
         self.ticker = ticker_symbol
 
-    def send(self, massage:[], change: float, update_code: int):
+    def send(self, articles:[], change: float, update_code: int):
 
         if update_code == DRAMATIC_CHANGE_CODE:
-            massage = f'Subject:{self.ticker.upper()}: {round(change)}%\n\n{massage[0]}\n\n{massage[1]}\n\n{massage[2]}\n\n'.encode("utf8")
+            massage = f'Subject:{self.ticker.upper()}: {round(change)}%\n\n{articles[0]}\n\n{articles[1]}\n\n{articles[2]}\n\n'.encode("utf8")
 
         elif update_code == DAILY_UPDATE_CODE:
-            massage = f'Subject:Daily update - {self.ticker.upper()}: {round(change)}%\n\n{massage[0]}\n\n{massage[1]}\n\n{massage[2]}\n\n'.encode("utf8")
+            massage = f'Subject:Daily update - {self.ticker.upper()}: {round(change)}%\n\n{articles[0]}\n\n{articles[1]}\n\n{articles[2]}\n\n'.encode("utf8")
 
         elif update_code == MONTHLY_UPDATE_CODE:
             massage = f'Subject:Monthly update - {self.ticker.upper()}, Change this month: {round(change)}%\n\n' \
-                      f'The most relevant news for this month:\n{massage[0]}\n\n{massage[1]}\n\n{massage[2]}\n\n'.encode("utf8")
+                      f'The most relevant news for this month:\n{articles[0]}\n\n{articles[1]}\n\n{articles[2]}\n\n'.encode("utf8")
 
         elif update_code == YEARLY_UPDATE_CODE:
-            massage = f'Subject:Yearly update - {self.ticker.upper()}, Change this month: {round(change)}%\n\n' \
-                      f'The most relevant news for this year:\n{massage[0]}\n\n{massage[1]}\n\n{massage[2]}\n\n'.encode("utf8")
+            massage = f'Subject:Yearly update - {self.ticker.upper()}, Change this year: {round(change)}%\n\n' \
+                      f'The most relevant news for this year:\n{articles[0]}\n\n{articles[1]}\n\n{articles[2]}\n\n'.encode("utf8")
 
         try:
             with smtplib.SMTP("smtp-mail.outlook.com") as connection:
